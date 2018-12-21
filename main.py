80884ad6b21335a65c6307eb4b8bcee0ee1de514
culring@windows <ravw96@gmail.com>
2018-12-21 22:39:29 +0100

    Nekotester refreshed.

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
-try:
-    from mytest import mytest
-except:
-    class mytest:
-        def neko_tester(files=0, time_limit=2.0, equal=None, generator=None, brute=None, path='0'):
-            def fun(f):
-                def wrapper(*args, **kwargs):
-                    f(*args, **kwargs)
-                return wrapper
-            return fun
+from math import gcd
+from mytest import mytest
 
 
 def brut():
-    pass
+    C = int(input())
+
+    for _ in range(C):
+        input()
+        number_of_floors = int(input())
+        rooms = [int(x) for x in input().split()]
+        X, Y = [int(x) for x in input().split()]
+
+        result = 0
+        for floor in range(1, number_of_floors+1):
+            l = 100*floor
+            r = 100*floor + rooms[floor-1]
+
+            for room in range(l+1, r+1):
+                if not room % X or not room % Y:
+                    result += 1
+
+        print(result)
 
 
-@mytest.neko_tester(100, brute=brut, equal=0, generator=0)
+def lcm(k, l):
+    return k*l/gcd(k, l)
+
+
+@mytest.neko_tester(0, path='10')
 def main():
-    pass
+    C = int(input())
+
+    for _ in range(C):
+        input()
+        number_of_floors = int(input())
+        rooms = [int(x) for x in input().split()]
+        X, Y = [int(x) for x in input().split()]
+        result = 0
+        for floor in range(1, number_of_floors+1):
+            r = 100*floor + rooms[floor-1]
+            l = 100*floor
+            result += r // X + r // Y - r // lcm(X, Y) - (l // X + l // Y - l // lcm(X, Y))
+        print(int(result))
+
 
 if __name__ == "__main__":
     main()
--- mytest/mytest.py
+++ mytest/mytest.py
@@ -5,10 +5,12 @@ import time
 
 def check(file_out, file_cor):
     file_out.seek(0)
-    for x, y in zip(enumerate(file_cor), enumerate(file_out)):
-        if x[1] != y[1]:
-            raise WrongAnswerException(f"Different in line {x[0]}:\nExpected:\t{x[1]}Got:\t\t{y[1]}")
-    return 'OK'
+
+    for i, lines in enumerate(zip(file_cor, file_out)):
+        if lines[0] != lines[1]:
+            raise WrongAnswerException(f"Different in line {i + 1}:\nExpected:\t{lines[0]}Got:\t\t{lines[1]}")
+
+    return "OK"
 
 
 class WrongAnswerException(Exception):
@@ -19,104 +21,81 @@ class TimeoutException(Exception):
     pass
 
 
-# #
-# # class MyTimer(Timer):
-# #     def __init__(self, *args):
-# #         Timer.__init__(self, *args)
-# #         self.ex = None
-# #
-# #     def run(self):
-# #         try:
-# #             super(MyTimer, self).run()
-# #         except Exception as e:
-# #             self.ex = e
-#
-# @contextmanager
-# def timer(seconds):
-#     def handler():
-#         raise TimeoutException("TIME LIMIT EXCEEDED[" + str(seconds) + " sec]")
-#
-#     t = Timer(seconds, handler)
-#     t.start()
-#     try:
-#         yield
-#     finally:
-#         t.cancel()
-#
+IN_FILENAME_SUFFIX = '_in.txt'
+OUT_FILENAME_SUFFIX = '_out.txt'
+COR_FILENAME_SUFFIX = '_cor.txt'
 
-def neko_tester(files, time_limit=2.0, equal=check, generator=None, brute=None, path='0'):
-    if equal == 0:
+
+def neko_tester(files, time_limit=2.0, equal='standard', generator=None, brute=None, path='0'):
+    if equal == 'standard':
+        equal = check
+
+    elif equal == 'no_check':
         def equal(x, y):
             return True
-    if generator == 0:
-        generator = []
-    if brute == 0:
-        def brute():
-            pass
+
     if type(files) is list:
         generator = None
 
     def fun(f):
         def wrapper(*args, **kwargs):
+            tests_directory = "tests/" + path + "/"
             tests = range(files) if type(files) is int else files
-            TESTS_LOCATION = "./tests/" + path + "/"
-            IN_FILENAME_SUFFIX = '_in.txt'
-            OUT_FILENAME_SUFFIX = '_out.txt'
-            COR_FILENAME_SUFFIX = '_cor.txt'
-            if not os.path.exists(TESTS_LOCATION):
-                os.makedirs(TESTS_LOCATION)
+            if not os.path.exists("tests/" + path):
+                os.makedirs("tests/" + path)
             count = 0
             backup = sys.stdin, sys.stdout
 
             for test_number in tests:
-                _in = TESTS_LOCATION + str(test_number) + IN_FILENAME_SUFFIX
-                _out = TESTS_LOCATION + str(test_number) + OUT_FILENAME_SUFFIX
-                _cor = TESTS_LOCATION + str(test_number) + COR_FILENAME_SUFFIX
-                with open(_in, 'r' if generator is None else 'w+') as file_in, \
-                        open(_out, 'w+') as file_out, \
-                        open(_cor, 'r' if brute is None else 'w+') as file_cor:
-                    try:
-                        if generator is not None:
-                            if type(generator) is list:
-                                for line in generator:
-                                    line = ''.join([str(x) + ' ' for x in line]) + '\n'
-                                    file_in.write(line)
-                            else:
-                                for line in generator():
-                                    file_in.write(str(line) + '\n')
-                            file_in.seek(0)
-                        if brute is not None:
-                            sys.stdin, sys.stdout = file_in, file_cor
-                            brute()
-                            file_cor.seek(0)
-                            file_in.seek(0)
+                _in = tests_directory + str(test_number) + IN_FILENAME_SUFFIX
+                _out = tests_directory + str(test_number) + OUT_FILENAME_SUFFIX
+                _cor = tests_directory + str(test_number) + COR_FILENAME_SUFFIX
 
-                        sys.stdin, sys.stdout = file_in, file_out
+                with open(_in, 'r' if generator is None else 'w+') as file_in:
+                    with open(_out, 'w+') as file_out:
+                        with open(_cor, 'r' if brute is None else 'w+') as file_cor:
+                            try:
+                                if generator is not None:
+                                    if type(generator) is list:
+                                        for line in generator:
+                                            line = ''.join([str(x) + ' ' for x in line]) + '\n'
+                                            file_in.write(line)
+                                    else:
+                                        for line in generator():
+                                            file_in.write(str(line) + '\n')
+                                    file_in.seek(0)
+                                if brute is not None:
+                                    sys.stdin, sys.stdout = file_in, file_cor
+                                    brute()
+                                    file_cor.seek(0)
+                                    file_in.seek(0)
 
-                        start = time.time()
-                        f(*args, **kwargs)
-                        period = time.time() - start
+                                sys.stdin, sys.stdout = file_in, file_out
 
-                        if period > time_limit:
-                            raise TimeoutException(str(period) + " sec\n")
-                        print(f'{test_number}: {equal(file_out, file_cor)}, TIME: {period}', file=sys.stderr)
+                                start = time.time()
+                                f(*args, **kwargs)
+                                period = time.time() - start
 
-                    except TimeoutException as e:
-                        print(f'{test_number}: TIME LIMIT EXCEEDED\nTIME: {e}\n###Passed[{count}]',
-                              file=sys.stderr, end='')
-                        break
-                    except WrongAnswerException as e:
-                        print(f'{test_number}: WRONG ANSWER\n{e}\n###Passed[{count}]',
-                              file=sys.stderr, end='')
-                        break
-                    except Exception as e:
-                        print(f'{test_number}: RUNTIME ERROR\n{type(e)}\n{e}\n###Passed[{count}]',
-                              file=sys.stderr, end='')
-                        raise e
-                    finally:
-                        count += 1
+                                if period > time_limit:
+                                    raise TimeoutException(str(period) + " sec\n")
+                                print(f'{test_number}: {equal(file_out, file_cor)}, TIME: {period}', file=sys.stderr)
+
+                                count += 1
+
+                            except TimeoutException as e:
+                                print(f'{test_number}: TIME LIMIT EXCEEDED\nTIME: {e}\n###Passed[{count}]',
+                                      file=sys.stderr, end='')
+                                break
+                            except WrongAnswerException as e:
+                                print(f'{test_number}: WRONG ANSWER\n{e}\n###Passed[{count}]',
+                                      file=sys.stderr, end='')
+                                break
+                            except TimeoutException as e:
+                                print(f'{test_number}: RUNTIME ERROR\n{type(e)}\n{e}\n###Passed[{count}]',
+                                      file=sys.stderr, end='')
+                                break
             else:
-                print(f'###SUCCEED[{count}]', file=sys.stderr, end='')
+                print(f'###SUCCESS[{count}]', file=sys.stderr, end='')
             sys.stdin, sys.stdout = backup
 
         return wrapper
